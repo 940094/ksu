7761fc9b6a725a2943ba5f61497dc8ede3077926
Justin Shaw <easyworkemail@gmail.com>
2019-05-23 11:02:56 -0400

    add exercises from codecademy

---
+++ practice/MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+  public static void main(String[] args) {
+    String name = "Justin";
+    int age = 24;
+    double desiredSalary = 100.00;
+    char gender = 'm';
+    boolean lookingForJob = true;
+  }
+}
