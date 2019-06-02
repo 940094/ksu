4a2f997d7238d1089644d77886139c301344b891
Justin Shaw <easyworkemail@gmail.com>
2019-06-02 11:42:46 -0400

    add exercises from ch.9

---
+++ book/programmingExercises/NineOne.java
@@ -0,0 +1,46 @@
+public class NineOne
+{
+  public static void main(String[] args)
+  {
+    Rectangle r1 = new Rectangle(4, 40);
+    Rectangle r2 = new Rectangle(3.5, 35.9);
+    System.out.println("Rectangle 1:");
+    System.out.println("height:\t" + r1.height);
+    System.out.println("width:\t" + r1.width);
+    System.out.println("area:\t" + r1.getArea());
+    System.out.println("perim:\t" + r1.getPerimeter());
+    System.out.println("\nRectangle 2:");
+    System.out.println("height:\t" + r2.height);
+    System.out.println("width:\t" + r2.width);
+    System.out.println("area:\t" + r2.getArea());
+    System.out.println("perim:\t" + r2.getPerimeter());
+  }
+}
+
+class Rectangle
+{
+  double height;
+  double width;
+
+  Rectangle()
+  {
+    height = 1;
+    width = 1;
+  }
+
+  Rectangle(double newWidth, double newHeight)
+  {
+    width = newWidth;
+    height = newHeight;
+  }
+
+  double getArea()
+  {
+    return height * width;
+  }
+
+  double getPerimeter()
+  {
+    return height * 2 + width * 2;
+  }
+}
